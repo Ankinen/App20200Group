53105078227785041f7467be57247c6706f25999
Nea Träskman <nea.traskman@gmail.com>
2022-12-13 15:51:51 +0200

    updated app.js and added index.js in screens

--- mobile/App.js
+++ mobile/App.js
@@ -1,10 +1,12 @@
+import Screens from './screens';
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +19,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 14,
+    fontWeight: 'bold',
+  }
 });
---
+++ mobile/screens/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+// importing screen components
+// import SignIn from './signin';
+// import SignUp from './signup';
+// import Settings from './settings';
+
+/*const TabNavigator = createBottomTabNavigator({
+    Settings: {
+        screen: SettingsStack,
+        navigationOptions: {
+          tabBarLabel: 'Settings',
+          tabBarIcon: ({ tintColor }) => (
+            <MaterialCommunityIcons name="settings" size={24} color={tintColor} />
+            )
+        }
+    }
+}); */
